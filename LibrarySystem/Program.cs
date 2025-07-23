b3dad4120fae3ac7a1034aad18ba6ab66629a367
SebasDev <sebastian.ramirez@blackbirdlabs.com.co>
2025-07-23 11:41:33 -0500

    feat: Implementacion del dockerfile

--- LibrarySystem/Program.cs
+++ LibrarySystem/Program.cs
@@ -52,11 +52,8 @@ using (var scope = app.Services.CreateScope())
 }
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 app.UseCors("AllowAll");
